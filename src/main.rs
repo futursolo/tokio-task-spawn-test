7578665b2294b03236772e2de98d6439a5f2f21f
Kaede Hoshikawa <futursolo@icloud.com>
2022-07-10 17:24:25 +0900

    Update tests.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 use std::time::{Duration, Instant};
 
 use futures::stream::{FuturesOrdered, StreamExt};
+use futures::Future;
 use tokio_util::task::LocalPoolHandle;
 
-#[global_allocator]
-static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-async fn task() {
-    tokio::time::sleep(Duration::ZERO).await;
-}
-
-#[tokio::main]
-async fn main() {
+async fn run_test<Fut>(f: fn() -> Fut)
+where
+    Fut: 'static + Send + Future<Output = ()>,
+{
     let pool = LocalPoolHandle::new(num_cpus::get());
 
     for round in 1..=5 {
-        println!("Round: {}", round);
+        println!("  Round: {}", round);
 
         // Futures Ordered.
         {
@@ -23,23 +19,26 @@ async fn main() {
             let mut furs = FuturesOrdered::new();
 
             for _ in 0..1_000_000 {
-                furs.push(task());
+                furs.push(f());
             }
 
             while furs.next().await.is_some() {}
 
-            println!("  futures ordered: {}ms", start_time.elapsed().as_millis());
+            println!(
+                "    futures ordered: {}ms",
+                start_time.elapsed().as_millis()
+            );
         }
 
         // spawn_local
         {
             let start_time = Instant::now();
 
-            pool.spawn_pinned(|| async {
+            pool.spawn_pinned(move || async move {
                 let mut handles = Vec::with_capacity(1_000_000);
 
                 for _ in 0..1_000_000 {
-                    handles.push(tokio::task::spawn_local(task()));
+                    handles.push(tokio::task::spawn_local(f()));
                 }
 
                 for handle in handles.into_iter() {
@@ -49,41 +48,59 @@ async fn main() {
             .await
             .expect("failed to complete.");
 
-            println!("      spawn local: {}ms", start_time.elapsed().as_millis());
+            println!(
+                "        spawn local: {}ms",
+                start_time.elapsed().as_millis()
+            );
         }
 
-        // spawn_pinned
+        // spawn_pinned to local pool
         {
             let start_time = Instant::now();
             let mut handles = Vec::with_capacity(1_000_000);
 
             for _ in 0..1_000_000 {
-                handles.push(pool.spawn_pinned(task));
+                handles.push(pool.spawn_pinned(f));
             }
 
             for handle in handles.into_iter() {
                 handle.await.expect("task failed.");
             }
 
-            println!("     spawn pinned: {}ms", start_time.elapsed().as_millis());
+            println!(
+                "       spawn pinned: {}ms",
+                start_time.elapsed().as_millis()
+            );
         }
 
-        // Spawn with Send runtime.
+        // Spawn with default runtime.
         {
             let start_time = Instant::now();
             let mut handles = Vec::with_capacity(1_000_000);
 
             for _ in 0..1_000_000 {
-                handles.push(tokio::task::spawn(task()));
+                handles.push(tokio::task::spawn(f()));
             }
 
             for handle in handles.into_iter() {
                 handle.await.expect("task failed.");
             }
 
-            println!("            spawn: {}ms", start_time.elapsed().as_millis());
+            println!(
+                "              spawn: {}ms",
+                start_time.elapsed().as_millis()
+            );
         }
 
         println!();
     }
+}
+
+#[tokio::main]
+async fn main() {
+    println!("Testing Ready Future...");
+    run_test(|| async {}).await;
+
+    println!("Testing sleep(Duration::ZERO) Future...");
+    run_test(|| tokio::time::sleep(Duration::ZERO)).await;
 }
